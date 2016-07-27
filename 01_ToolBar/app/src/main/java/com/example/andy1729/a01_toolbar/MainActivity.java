5815a4b02962a77f16b19ac39a1f634bb9abb084
andy1729 <ujjawalanand1729@gmail.com>
2016-07-27 23:26:04 +0530

    Added Toolbar

---
+++ 01_ToolBar/app/src/main/java/com/example/andy1729/a01_toolbar/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.andy1729.a01_toolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        //Display icon in toolbar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.icon_like_red_small);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
